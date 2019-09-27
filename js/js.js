15a989adbba281a52e946533600314d230b8588a
Tom Newby <tnewby@salisbury6c.ac.uk>
2019-09-27 11:17:48 +0100

    All the work

---
+++ js/js.js
@@ -0,0 +1,17 @@
+function sphere(){
+    var radius = document.getElementById("sphereId").value;
+    var result = (4/3)*Math.PI*radius**3;
+    document.getElementById("h1").innerHTML = "Sphere volume: "+result;
+}
+
+function cube(){
+    var length = document.getElementById("cubeId").value;
+    var result = length**3;
+    document.getElementById("h2").innerHTML = "Cube volume: "+result;
+}
+
+function triangle(){
+    var base = document.getElementById("triangleId1").value;
+    var height = document.getElementById("triangleId2").value;
+    document.getElementById("h3").innerHTML = "Triangle area: "+(base/2)*height;
+}
